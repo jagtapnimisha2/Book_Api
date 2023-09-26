97915fbb07a5eb872925c2f3e55c624c434b2758
jagtapnimisha2 <120504401+jagtapnimisha2@users.noreply.github.com>
2023-09-26 12:13:01 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
+import { AppProvider } from './context.';
+import './index.css';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "about" element = {<About />} />
+          <Route path = "book" element = {<BookList />} />
+          <Route path = "/book/:id" element = {<BookDetails />} />
+          
+        </Route>
+        
+      </Routes>
+      <footer class="footer-distributed">
+
+<div class="footer-left">
+    <h3><span>Developer</span></h3>
+
+    <p class="footer-links">
+        <a href="#">Home</a>
+        |
+        <a href="#">About</a>
+        |
+        <a href="#">Contact</a>
+        |
+        <a href="#">Blog</a>
+    </p>
+
+    <p class="footer-company-name">Copyright © 2021 <strong>Developer</strong> All rights reserved</p>
+</div>
+
+<div class="footer-center">
+    <div>
+        <i class="fa fa-map-marker"></i>
+        <p><span>Ghaziabad</span>
+            Delhi</p>
+    </div>
+
+    <div>
+        <i class="fa fa-phone"></i>
+        <p>+91 74**9**258</p>
+    </div>
+    <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="mailto:sagar00001.co@gmail.com">xyz@gmail.com</a></p>
+    </div>
+</div>
+<div class="footer-right">
+    <p class="footer-company-about">
+        <span>About the company</span>
+        <strong>Developer</strong> is a Youtube channel where you can find more creative CSS Animations
+        and
+        Effects along with
+        HTML, JavaScript and Projects using C/C++.
+    </p>
+    <div class="footer-icons">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-youtube"></i></a>
+    </div>
+</div>
+</footer>
+    </BrowserRouter>
+ 
+  </AppProvider>
+);
+
